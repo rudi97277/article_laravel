7d3d63907ced9ea0546d8fb82d29008134501e85
Rudianto Sihombing <45420193+rudi97277@users.noreply.github.com>
2023-10-31 22:54:24 +0700

    Update MembershipResource.php
    Add created at

--- app/Http/Resources/MembershipResource.php
+++ app/Http/Resources/MembershipResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class MembershipResource extends JsonResource
@@ -22,7 +23,8 @@ class MembershipResource extends JsonResource
             'link_schooler' => $this->link_schooler,
             'link_scoopus' => $this->link_scoopus,
             'verified' => (bool)$this->verified,
-            'evidence' => new DocumentResource($this->evidence)
+            'evidence' => new DocumentResource($this->evidence),
+            'created_at' => Carbon::parse($this->created_at)->format('Y-m-d H:i:s')
         ];
     }
 }
